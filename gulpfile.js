56d95eab3f1f5fc99e7bfde18bcbda523d0d4758
Al Tsaplin <al.tsaplin@gmail.com>
2023-04-18 23:44:21 +0300

    add: some files\folders structure and gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+const gulp = require("gulp");
+const plumber = require('gulp-plumber');
+const sourcemap = require('gulp-sourcemaps');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const sync = require('browser-sync').create();
+
+// обработка файлов стилей
+const styles = () => {
+  return gulp
+    .src('source/sass/style.scss') // считывает корневой (главный) SCSS-файл со всеми вложенностями.
+    .pipe(plumber()) // отлавливает ошибки, если будут
+    .pipe(sourcemap.init()) // включает карты кода (сборка)
+    .pipe(sass()) // обрабатываем код в CSS с помощью препроцессора SASS
+    .pipe(postcss([autoprefixer()])) // обработка CSS добавляет нужные префиксы
+    .pipe(sourcemap.write('.')) // карты кода (запись)
+    .pipe(gulp.dest('source/css')) // результат обработанного кода сохраняется
+    .pipe(sync.stream()); // обновляет изменения с запущенным локальным сервером
+};
+// запуск локального сервера и открытие проекта в браузере
+const server = () => {
+  sync.init({
+    server: {
+      baseDir: 'source'
+    },
+    cors: true,
+    notify: false,
+    ui: false,
+  });
+};
+// слежка за изменением файлов HTML и стилей
+const watcher = () => {
+  gulp.watch('source/sass/**/*.scss', gulp.series('styles'));
+  gulp.watch('source/*.html').on('change', sync.reload);
+};
+
+exports.default = gulp.series(
+  styles, server, watcher
+);
